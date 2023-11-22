9253c9845ec9ffaf53a23ae36bea4e12db498a0e
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2023-11-22 14:42:32 -0500

    points edits
    the svgs in points completed and every section placed in its own component and brought into home section
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./reviews.styles.scss";
+
+const Reviews = () => {
+  return (
+    <>
+      <div className="reviews-container">
+        <h2 className="reviews-header">Reviews</h2>
+        <div className="reviews">
+          <div className="reviews-text">
+            <b> Review from South Portland, Maine:</b> <br />
+            Rating:
+            <i> 5/5 stars</i> <br />
+            "Living in South Portland, where winters can be long and harsh, I
+            appreciate The Plant Haus for offering a variety of hardy plants
+            that can thrive in our climate. The pet-friendly selection is
+            fantastic too! The delivery was prompt, and the plants arrived in
+            excellent condition. I'm now a loyal customer!"
+          </div>
+          <div className="reviews-text">
+            <b>Review from Austin, Texas:</b> <br />
+            <i> 4.5/5 stars</i> <br />
+            "The Plant Haus' online support team is incredible. They helped me
+            navigate the unique care requirements for each plant. I deducted 1/2
+            a star because I'd love to see even more options for those of us in
+            warmer regions. Overall I would highly reccommend!"
+          </div>
+          <div className="reviews-text">
+            <b>"Review from Salt Lake City, Utah:</b> <br />
+            <i> 4.5/5 stars</i> <br />
+            "Absolutely thrilled with my recent plant purchase from The Plant
+            Haus! Living in Salt Lake City, finding the right plants can be a
+            challenge, but The Plant Haus delivered plants that work in my
+            environment perfectly. The packaging was excellent and their
+            commitment to quality truly sets them apart."
+          </div>
+          <div className="reviews-text">
+            <b>"Review from Brooklyn, New York:</b> <br />
+            <i> 4/5 stars</i> <br />
+            "As a Brooklyn plant parent, I'm always on the lookout for unique
+            additions for my collection. The Plant Haus didn't disappoint! The
+            selection is diverse, and the online support rocks. I knocked off
+            one star because I wish the delivery could be a bit quicker, but the
+            wait was well worth it."
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Reviews;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,12 @@
 import React from "react";
-import { LiaShippingFastSolid } from "react-icons/lia";
-import { PiPottedPlant, PiChatsCircle } from "react-icons/pi";
-import "../components/slider.styles.scss";
-import "./home.styles.scss";
+import Commitments from "../components/Commitments";
+import NewArrivals from "../components/NewArrivals";
+import PetFriendly from "../components/PetFriendly";
+import Reviews from "../components/Reviews";
 import heroImage from "../assets/styled-plants-5.png";
-import Monstera from "../assets/monstera-4.png";
-import SpiderPlant from "../assets/spider-plant-4.png";
-import SnakePlant from "../assets/snake-plant-3.png";
-import FiddleLeaf from "../assets/fiddle-leaf-fig-3.png";
-import Peperomia from "../assets/peperomia-3.png";
-import PrayerPalm from "../assets/prayer-palm-1.png";
-import BirdsNest from "../assets/birds-nest-fern-1.png";
-import Aloe from "../assets/aloe-vera-3.png";
-import Blob1 from "../assets/blobs/Blob1";
+import "./home.styles.scss";
 
 const Home = () => {
-  const pointsData = [
-    {
-      icon: <LiaShippingFastSolid size={40} />,
-      header: "Unbeatable Quality",
-      info: "Directly sourced from top-rated growers, our online plant shop offers an unparalleled selection of the finest quality plants at the most competitive prices.",
-      svg: <Blob1 />,
-    },
-    {
-      icon: <PiPottedPlant size={40} />,
-      header: "Effortless Doorstep Delivery",
-      info: "Experience the convenience of having your chosen plants delivered directly to your door when you shop with us online. Should your satisfaction ever waver, just let us know – we're committed to ensuring you're 100% happy with your purchase!",
-      svg: <Blob1 />,
-    },
-    {
-      icon: <PiChatsCircle size={40} />,
-      header: "Comprehensive Online Support",
-      info: "Upon ordering, you'll receive a complimentary plant-parenting instruction card. Plus, our team of virtual plant doctors is always on call, providing continuous online support for your plant care inquiries.",
-      svg: <Blob1 />,
-    },
-  ];
-
   return (
     <>
       {/* Hero section */}
@@ -55,45 +26,18 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Three Points of supprt section */}
-      <div className="three-points-container">
-        <div className="three-header">Our Commitment</div>
-        <div className="three-points-row ">
-          {pointsData.map((point, index) => (
-            <div className={`points point-${index}`} key={index}>
-              <div className="point-blob">{point.svg}</div>
-              <div className="point-icon">{point.icon}</div>
-              <h3 className="point-header">{point.header}</h3>
-              <p className="point-info">{point.info}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      {/* Our Commitments Section */}
+      <Commitments />
 
       {/* New Arrivals Section */}
-      <div className="new-arrivals-container">
-        <h2>New Arrivals</h2>
-        <div className="new-arrivals-images">
-          <img src={Monstera} alt={"monstera"} />
-          <img src={SpiderPlant} alt={"spider plant"} />
-          <img src={SnakePlant} alt={"snake plant"} />
-          <img src={FiddleLeaf} alt={"fiddle leaf fig"} />
-        </div>
-      </div>
+      <NewArrivals />
 
       {/* Pet Friendly Plants Section */}
-      <div className="pet-friendly-container">
-        <h2>Pet Friendly Plants</h2>
-        <div className="pet-friendly-images">
-          <img src={Peperomia} alt={"peperomia"} />
-          <img src={PrayerPalm} alt={"prayer palm"} />
-          <img src={BirdsNest} alt={"birst nest fern"} />
-          <img src={Aloe} alt={"aloe vera"} />
-        </div>
-      </div>
+
+      <PetFriendly />
 
       {/* Reviews */}
-      <div className="reviews-container"></div>
+      <Reviews />
     </>
   );
 };
