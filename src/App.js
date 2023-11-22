7599e5c12eda41b21275c054fae77acefdc1f078
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2023-11-22 16:36:10 -0500

    Mystery Semicolon Bug Fixed
    Random semicolon appearing at bottom of the screen and found in the root folder, finally appeared in code after adding a .prettierrc file where we took away all semi colons.  Showed up in the App.js file.
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/home/Home"
-import Shop from "./pages/shop/Shop"
-import About from "./pages/about/About"
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Shop from './pages/shop/Shop'
+import About from './pages/about/About'
 
 const App = () => {
   return (
@@ -12,7 +12,6 @@ const App = () => {
           <Route path="/about" element={<About />} />
         </Route>
       </Routes>
-      ;
     </>
   )
 }
