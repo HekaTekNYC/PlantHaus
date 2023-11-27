54d935fa83a30e0f1a4284a7f54d232365bb88ce
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2023-11-27 15:15:20 -0500

    checkout page creation
    checkout page working with add to cart and remove cart properly incrementing and decrementing
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/components/product-card/product-card.jsx
+++ src/components/product-card/product-card.jsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react'
 import Button from '../button/Button'
 import './product-card.styles.scss'
+import { CartContext } from '../../contexts/cart.context'
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product
+  const { addItemToCart } = useContext(CartContext)
+  const addProductToCart = () => addItemToCart(product)
+
   return (
     <>
       <div className="product-card-container">
         <div className="img-btn-container">
           <img src={imageUrl} alt={`${name}`} />{' '}
-          <Button buttonType="inverted">Add to cart</Button>
+          <Button buttonType="inverted" onClick={addProductToCart}>
+            Add to cart
+          </Button>
         </div>
         <div className="footer">
           <span className="text">{name}</span>
