9c6a796be8abf80cf9bc23bf4d505a9731b63b79
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2023-12-01 12:15:16 -0500

    styling edits
    cart styling edits and media queries on home page
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/components/product-card/product-card.jsx
+++ src/components/product-card/product-card.jsx
@@ -11,7 +11,9 @@ const ProductCard = ({ product }) => {
   return (
     <>
       <div className="product-card-container">
-        <img src={imageUrl} alt={`${name}`} />
+        <div className="product-img">
+          <img src={imageUrl} alt={`${name}`} />
+        </div>
         <div className="footer">
           <span className="name">{name}</span>
           <span className="price">${price}</span>
