8bb97a705098052044b4f7503d32bf8e8e3d0b8f
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-01-20 13:54:33 -0500

    category preview and mobile fix
    category preview responsiveness fixed with overlay on mobile - title moved to middle of card with only one card showing.

--- src/components/category-preview/category-preview.jsx
+++ src/components/category-preview/category-preview.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import shopTitle from '../../utils/shopTitle.utils'
 import ProductCard from '../product-card/product-card'
+import { IoIosArrowForward } from 'react-icons/io'
 
 import './category-preview.styles.scss'
 
@@ -10,21 +11,37 @@ const CategoryPreview = ({ title, products }) => {
 
   return (
     <>
-  
-          <div className="category-preview-container">
-      <h2>
-        <Link className="title" to={`/shop/${title}`}>
-          {shopTitle(title)}
-        </Link>
-      </h2>
-      <div className="preview">
-        {products
-          .filter((_, idx) => idx < 4)
-          .map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+      <div className="category-preview-container">
+        <h2>
+          <Link className="title" to={`/shop/${title}`}>
+            {shopTitle(title)}
+          </Link>
+        </h2>
+
+        <div className="preview">
+          {products
+            .filter((_, idx) => idx < 4)
+            .map((product) => (
+              <div className="product-wrapper" key={product.id}>
+                <ProductCard product={product} />
+                <div className="category-overlay">
+                  <h3>{shopTitle(title)}</h3>
+                </div>
+              </div>
+            ))}
+          <Link className="title" to={`/shop/${title}`}>
+            <div className="view-more-card">
+              <div className="view-more-text">
+                View <br />
+                More
+                <div className="arrow-icon">
+                  <IoIosArrowForward />
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
-    </div>
     </>
   )
 }
