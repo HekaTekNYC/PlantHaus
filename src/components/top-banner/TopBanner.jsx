3d128f78e447f84efc7475a9b6ba3b4c1f2645f2
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-04-23 22:23:38 -0400

    top banner created

---
+++ src/components/top-banner/TopBanner.jsx
@@ -0,0 +1,11 @@
+import './top-banner.styles.scss'
+const TopBanner = () => {
+  return (
+    <div className="top-banner">
+      Free shipping on all orders over $75{' '}
+      <span className="top-banner-extend"> + 30 Day Guarantee</span>
+    </div>
+  )
+}
+
+export default TopBanner
